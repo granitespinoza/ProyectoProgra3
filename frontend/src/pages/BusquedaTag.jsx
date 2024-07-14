b01fcf22729c1a16b5736ff4e0edb18baf88bb84
Elvis Alejandro Lopez Gambini <elvis_lg06@hotmail.com>
2024-07-13 23:41:04 -0500

    Add page BusquedaTag

---
+++ frontend/src/pages/BusquedaTag.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusquedaTag = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-3xl font-bold">Pantalla funcional</h1>
+        </div>
+    );
+};
+
+export default BusquedaTag;
